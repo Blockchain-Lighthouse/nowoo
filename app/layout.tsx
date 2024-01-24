0d5849a7c55cc99a317800c2cf98b0efd139a820
w00khyung <qpflapffhs76@gmail.com>
2024-01-24 11:34:00 +0900

    head에 favicon 관련 메타 태그 추가함.

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' href='/favicon.ico' sizes='any' />
+      </head>
       <body className={cn(pretendard.className, 'min-h-dvh')}>
         <VercelAnalytics />
         <GoogleAnalytics />
