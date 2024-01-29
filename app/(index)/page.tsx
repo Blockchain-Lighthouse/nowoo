61fb5cccc05a9d6d70ac52e4b705d51de50edbe1
HyungWookChoi <qpflapffhs76@gmail.com>
2024-01-29 21:37:13 +0900

    access log 24시간 이내는 업데이트, 이후 신규 생성으로 로직 수정함.

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -14,10 +14,27 @@ export default async function HomePage() {
   const [ip] = (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')
   const userAgent = header.get('user-agent') ?? ''
 
-  await supabase.from('user_access').insert({
-    ip,
-    agent: userAgent,
-  })
+  const { data: prevAccessLogs } = await supabase
+    .from('user_access')
+    .select('id')
+    .match({ ip, agent: userAgent })
+    .gte('updated_dt', new Date(Date.now() - 24 * 60 * 60 * 1000).toUTCString())
+    .order('updated_dt', { ascending: false })
+
+  const lastAccessLog = prevAccessLogs?.[0]
+
+  if (lastAccessLog?.id) {
+    await supabase.from('user_access').upsert({
+      id: lastAccessLog.id,
+      ip,
+      agent: userAgent,
+    })
+  } else {
+    await supabase.from('user_access').insert({
+      ip,
+      agent: userAgent,
+    })
+  }
 
   return (
     <section className='mx-auto flex max-w-7xl flex-col items-center gap-6 p-24 max-lg:px-4'>
