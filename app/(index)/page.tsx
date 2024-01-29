8a428096c1d1b9fbf8decfdd2b4d66ef5cda5a97
HyungWookChoi <qpflapffhs76@gmail.com>
2024-01-29 20:57:34 +0900

    user access insert로 수정함.

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -13,15 +13,11 @@ export default async function HomePage() {
   const header = headers()
   const [ip] = (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')
   const userAgent = header.get('user-agent') ?? ''
-  const { data: userAccess } = await supabase.from('user_access').select('id').match({ ip }).single()
 
-  if (!userAccess?.id) {
-    await supabase.from('user_access').upsert({
-      id: userAccess?.id,
-      ip,
-      agent: userAgent,
-    })
-  }
+  await supabase.from('user_access').insert({
+    ip,
+    agent: userAgent,
+  })
 
   return (
     <section className='mx-auto flex max-w-7xl flex-col items-center gap-6 p-24 max-lg:px-4'>
